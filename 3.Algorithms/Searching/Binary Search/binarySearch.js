5309b87a69d9ba067c1207739394f1a0b798cc0a
AkshayGengaje <akshay@ai4mtech.com>
2023-09-22 21:59:48 +0530

    binarySearch.js implemented

--- 3.Algorithms/Searching/Binary Search/binarySearch.js
+++ 3.Algorithms/Searching/Binary Search/binarySearch.js
@@ -16,9 +16,34 @@
 function binarySearch(arr, item) {
   /* 2. Find the middle of the array
     Formula - M =  {L+R/2} or M = {L + (R - L) / 2}
+    Formula - Mid =  {(start+end)/2} or Mid = {start + (end - start) / 2} <= this is done for big value which can be greater than int size
   */
- 
+  // 3. We will run the loop till start and end are equal to each other
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = start + (end - start) / 2;
+
+  while (start <= end) {
+    if (arr[mid] === item) {
+      return mid;
+    }
+    // shift to left
+    //shift to right
+    else if (item > arr[mid]){
+      start = mid + 1;
+    }
+    else  {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  return -1;
 }
 
 // 1. the array must be already sorted any format, that is, either ascending or descending
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+//             0  1  2  3  4  5  6  7  8  9   10  11  12  13  14
+
+const ans = binarySearch(array, 13);
+
+console.log(ans);
